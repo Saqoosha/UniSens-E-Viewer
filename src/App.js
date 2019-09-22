c87d1ef48ab520f565082e9b170be2c58808b96c
Saqoosha <a@saqoo.sh>
2019-09-22 22:38:25 +0900

    First prototype

--- src/App.js
+++ src/App.js
@@ -1,26 +1,82 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useCallback } from "react"
+import { useDropzone } from 'react-dropzone'
+import Chart from "react-apexcharts"
+import * as Papa from "papaparse"
 
-function App() {
+
+export default props => {
+  const [options, setOptions] = useState({
+    chart: {
+      id: "basic-bar"
+    },
+    stroke: {
+      width: 2
+    },
+    xaxis: {
+      type: 'numeric',
+      tickAmount: 10,
+    },
+    yaxis: {
+      min: 0,
+      max: 2400,
+      tickAmount: 4,
+      decimalsInFloat: 0,
+    }
+  })
+
+  const [series, setSeries] = useState([])
+
+  // useEffect(() => {
+  //   Papa.parse("2019-09-21 SM UniLog 2 Datei 0004.txt", {
+  //     download: true,
+  //     dynamicTyping: true,
+  //     complete: ({ data, errors, meta }) => {
+  //       data.shift() // skip header
+  //       console.log(data[1], errors[0], meta)
+  //       let W = data.map(d => [d[3], d[8] || 0])
+  //       let i = W.length - 1
+  //       for (; i >= 0; i--) {
+  //         if (W[i][1] > 10.0) {
+  //           break
+  //         }
+  //       }
+  //       W = W.slice(0, i + 1)
+  //       setSeries([{ name: "W", data: W }])
+  //     }
+  //   })
+  // }, [])
+
+  const onDrop = useCallback(acceptedFiles => {
+    console.log(acceptedFiles)
+    Papa.parse(acceptedFiles[0], {
+      dynamicTyping: true,
+      complete: ({ data, errors, meta }) => {
+        data.shift() // skip header
+        console.log(data[1], errors[0], meta)
+        let W = data.map(d => [d[3], d[8] || 0])
+        let i = W.length - 1
+        for (; i >= 0; i--) {
+          if (W[i][1] > 20.0) {
+            break
+          }
+        }
+        W = W.slice(0, i + 1)
+        console.log(W)
+        setSeries([{ name: "W", data: W }])
+      }
+    })
+  }, [])
+  const { getRootProps, isDragActive } = useDropzone({ onDrop, accept: '.txt' })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app" {...getRootProps()}>
+      <Chart
+        options={options}
+        series={series}
+        type="line"
+        width="100%"
+        height="100%"
+      />
     </div>
-  );
+  )
 }
-
-export default App;
